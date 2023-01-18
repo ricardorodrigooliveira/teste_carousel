96e0887fdf05939f3ce7e76d8b86307c397d3da5
Ricardo Rodrigo de Oliveira <ricardorodrigo.oliveira@gmail.com>
2023-01-17 21:56:40 -0300

    Exemplo de carousel no react e typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
+import { useState, useEffect, useRef } from 'react';
 import './App.css';
+import{ motion } from 'framer-motion';
+
+import image1 from '../src/images/1.jpg';
+import image2 from '../src/images/2.jpg';
+import image3 from '../src/images/3.jpg';
+import image4 from '../src/images/4.jpg';
+
+const images = [image1, image2, image3, image4];
+
 
 function App() {
+  const carousel = React.useRef(HTMLElement.arguments);
+  const [width, setWidth] = useState(0);
+
+  useEffect(() => {
+    console.log(carousel.current?.scrollWidth, carousel.current?.offsetWidth, HTMLElement.arguments)
+    setWidth(carousel.current?.scrollWidth - carousel.current?.offsetWidth)
+  }, [])
+    
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <motion.div ref={carousel} className="carousel" whileTap={{ cursor: "grabbing" }}>
+        <motion.div className="inner" drag="x" dragConstraints={{right:0, left:-width}}>
+          {images.map(image => (
+              <motion.div className="item" key={image}>
+                <img src={image}/>
+              </motion.div>
+            ))}
+        </motion.div>
+      </motion.div>
     </div>
   );
 }
